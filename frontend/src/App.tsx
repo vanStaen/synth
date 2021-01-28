38eb66c297bab01d6c869d5a8ae43c035fc8b4cc
Clément van Staen <clement.vanstaen@gmail.com>
2021-01-28 14:47:25 +0100

    reinstall react for typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import Keyboard from './component/keyboard/Keyboard.tsx';
+import Keyboard from './component/keyboard/Keyboard';
 
 import './App.css';
 
@@ -7,6 +7,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <Keyboard/>
       </header>
     </div>
