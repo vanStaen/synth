96b66ce31a984c8805934bc262f653ad4c8fef7d
Clément van Staen <clement.vanstaen@gmail.com>
2021-01-28 13:29:55 +0100

    install typescript + create a keyboard component

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # synth
 Software synth using the Javascript audio.API
+
+## Setup
+
+Created with `npm create-react-app`, then added Typescript support (https://create-react-app.dev/docs/adding-typescript/).
+```
+npm install --save typescript @types/node @types/react @types/react-dom @types/jest
+```
+
+## Design
+
+### Keyboard : 
+https://codepen.io/zastrow/pen/oDBki
---
+++ frontend/src/component/keyboard/Keyboard.tsx
@@ -0,0 +1,23 @@
+import './Keyboard.css'; 
+
+const Keyboard = () => {
+
+    return (
+        <ul className="set">
+            <li className="white b"></li>
+            <li className="black as"></li>
+            <li className="white a"></li>
+            <li className="black gs"></li>
+            <li className="white g"></li>
+            <li className="black fs"></li>
+            <li className="white f"></li>
+            <li className="white e"></li>
+            <li className="black ds"></li>
+            <li className="white d"></li>
+            <li className="black cs"></li>
+            <li className="white c"></li>
+        </ul>
+    )
+}
+
+export default Keyboard;
