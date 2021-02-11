9eac05c478205c240acec3e153ece1c1bc106c72
Clément van Staen <clement.vanstaen@gmail.com>
2021-02-11 13:10:48 +0100

    try to generate white noise

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,57 @@
-import logo from './logo.svg';
 import Keyboard from './component/keyboard/Keyboard';
 
 import './App.css';
 
 function App() {
+
+  const audioContext = new AudioContext();
+
+  // First paramter = number of chanel (1 for mono, 2 for duo <-> Stero or polyphony)
+  const buffer = audioContext.createBuffer(
+    1,
+    audioContext.sampleRate * 1,
+    audioContext.sampleRate,
+  )
+
+  // Represente 1 second of audio. 
+  const channelData = buffer.getChannelData(0);
+
+  // The lengh of the array contained in channelData is equal to the sample rate
+  console.log(channelData.length);
+  console.log(audioContext.sampleRate);
+
+
+  // Create white noise by assigning random value between -1 and 1
+  for (let i= 0; i > buffer.length;i++) {
+    channelData[i] = Math.random() * 2 - 1;
+  }
+
+  // Create a Buffer Source
+  const whiteNoiseSource = audioContext.createBufferSource();
+  whiteNoiseSource.buffer = buffer;
+
+  // Create a Gain control (Master Volumne)
+  const primaryGainControl= audioContext.createGain();
+  primaryGainControl.gain.setValueAtTime(0.05, 0);
+
+  // Link Audio Source to Gain control
+  whiteNoiseSource.connect(primaryGainControl);
+  primaryGainControl.connect(audioContext.destination);
+
+  const playWhiteNoiseHandler = () => {
+    console.log("Resume audioContext");
+    audioContext.resume();
+    whiteNoiseSource.start();
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Synth
         </p>
+        <button onClick={playWhiteNoiseHandler}>White Noise</button>
         <Keyboard/>
       </header>
     </div>
