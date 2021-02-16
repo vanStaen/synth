d5e190e30afb7760b31a2b4d58f460f82ecc8183
Clément van Staen <clement.vanstaen@gmail.com>
2021-02-16 18:11:39 +0100

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,43 +13,67 @@ const App = () => {
     audioContext.sampleRate,
   )
 
-  // Represente 1 second of audio. 
-  const channelData = buffer.getChannelData(0);
-
-  // The lengh of the array contained in channelData is equal to the sample rate
-  console.log(channelData.length);
-  console.log(audioContext.sampleRate);
-
-  // Create white noise by assigning random value between -1 and 1
-  for (let i= 0; i > buffer.length;i++) {
-    channelData[i] = Math.random() * 2 - 1;
-  }
-
-  // Create a Gain control (Master Volumne)
+  // Create a Gain control (Master Volume)
   const primaryGainControl= audioContext.createGain();
   primaryGainControl.gain.setValueAtTime(0.05, 0);
   primaryGainControl.connect(audioContext.destination);
 
-  const playWhiteNoiseHandler = () => {
-    //Resume audioContext when clicking button
+
+  // Create a filter (LP)
+  const primaryfilter = audioContext.createBiquadFilter();
+  primaryfilter.type = "lowpass";
+  primaryfilter.frequency.value = 1500;
+  primaryfilter.connect(primaryGainControl);
+
+
+  const playWhiteNoiseHandler = () => {   
+
+      // Represente 1 second of audio. 
+      const channelData = buffer.getChannelData(0);
+
+      // Create white noise by assigning random value between -1 and 1
+      for (let i = 0; i < buffer.length;i++) {
+        channelData[i] = Math.random() * 2 - 1;
+      }  
+      //Resume audioContext when clicking button
+      console.log("Resume audioContext");
+      audioContext.resume();
+      //Create a Buffer Source
+      const whiteNoiseSource = audioContext.createBufferSource();
+      whiteNoiseSource.buffer = buffer;
+      //Link Audio Source to Gain control
+      whiteNoiseSource.connect(primaryfilter);
+      whiteNoiseSource.start();
+
+  }
+
+  const playNoteHandler = () => {   
+
+    // const playNoteHandler = (note: string) => {   
+    // console.log("note", note);
+
+    // Resume audioContext when clicking button
     console.log("Resume audioContext");
     audioContext.resume();
-    //Create a Buffer Source
-    const whiteNoiseSource = audioContext.createBufferSource();
-    whiteNoiseSource.buffer = buffer;
-    //Link Audio Source to Gain control
-    whiteNoiseSource.connect(primaryGainControl);
-    whiteNoiseSource.start();
-  }
+
+    // Create a Buffer Source
+    const sinOscillator = audioContext.createOscillator();
+    sinOscillator.frequency.setValueAtTime(261.6, 0); // Here convert to note
+
+    // Link Audio Source to Gain control
+    sinOscillator.connect(primaryfilter);
+    sinOscillator.start();
+    sinOscillator.stop(audioContext.currentTime + 1); // will play for one second.
+}
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Synth
+          Synth 
         </p>
         <button onClick={playWhiteNoiseHandler}>White Noise</button>
-        <Keyboard/>
+        <Keyboard playNoteHandler={playNoteHandler}/>
       </header>
     </div>
   );
