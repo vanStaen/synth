0df85e54b02752ce2f92a37e571558e48c53c529
Clément van Staen <clement.vanstaen@gmail.com>
2021-02-16 18:11:03 +0100

    keyboard update

--- frontend/src/component/keyboard/Keyboard.tsx
+++ frontend/src/component/keyboard/Keyboard.tsx
@@ -1,21 +1,25 @@
 import './Keyboard.css'; 
 
-const Keyboard = () => {
+type KeyboardProps = {
+    playNoteHandler: (event: React.MouseEvent<HTMLElement>) => void;
+  }
+  
+const Keyboard = ({playNoteHandler}: KeyboardProps) => {
 
     return (
         <ul className="set">
-            <li className="white b"></li>
-            <li className="black as"></li>
-            <li className="white a"></li>
-            <li className="black gs"></li>
-            <li className="white g"></li>
-            <li className="black fs"></li>
-            <li className="white f"></li>
-            <li className="white e"></li>
-            <li className="black ds"></li>
-            <li className="white d"></li>
-            <li className="black cs"></li>
-            <li className="white c"></li>
+            <li key="F" className="white b" onClick={playNoteHandler}></li>
+            <li key="F#" className="black as" onClick={playNoteHandler}></li>
+            <li key="G" className="white a" onClick={playNoteHandler}></li>
+            <li key="G#" className="black gs" onClick={playNoteHandler}></li>
+            <li key="A" className="white g" onClick={playNoteHandler}></li>
+            <li key="A#" className="black fs" onClick={playNoteHandler}></li>
+            <li key="B" className="white f" onClick={playNoteHandler}></li>
+            <li key="C" className="white e" onClick={playNoteHandler}></li>
+            <li key="C#" className="black ds" onClick={playNoteHandler}></li>
+            <li key="D" className="white d" onClick={playNoteHandler}></li>
+            <li key="D#" className="black cs" onClick={playNoteHandler}></li>
+            <li key="E" className="white c" onClick={playNoteHandler}></li>
         </ul>
     )
 }
