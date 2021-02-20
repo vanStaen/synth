e94f1fea5bd19a3b97f1e90d46850ed65ff21e33
Clément van Staen <clement.vanstaen@gmail.com>
2021-02-20 18:54:44 +0100

    square osc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,11 @@ import Knob from "./component/knob/Knob";
 import "./App.css";
 
 const App = () => {
-  const [octave, setOctave] = useState(4);
+  const [octave, setOctave] = useState(3);
   const [mainVolume, setMainVolume] = useState(0.05);
-  const [noiseVolume, setNoiseVolume] = useState(.7);
-  const [sineVolume, setSineVolume] = useState(1);
+  const [noiseVolume, setNoiseVolume] = useState(.1);
+  const [sineVolume, setSineVolume] = useState(.3);
+  const [squareVolume, setSquareVolume] = useState(.6);
   const [fitlerFreq, setFitlerFreq] = useState(5000); // from 25hz to 25000hz
 
   const audioContext = new AudioContext();
@@ -39,11 +40,23 @@ const App = () => {
       //Sinus
       const sinGain = audioContext.createGain();
       sinGain.gain.setValueAtTime(sineVolume, 0);
-      sinGain.connect(primaryfilter);
+      sinGain.connect(noteGain);
       const sinOscillator = audioContext.createOscillator();
       sinOscillator.frequency.setValueAtTime(freq, 0);
-      sinOscillator.connect(noteGain);
+      sinOscillator.type = 'sine';
+      sinOscillator.connect(sinGain);
       sinOscillator.start();
+
+
+      //Square
+      const squareGain = audioContext.createGain();
+      squareGain.gain.setValueAtTime(squareVolume, 0);
+      squareGain.connect(noteGain);
+      const squareOscillator = audioContext.createOscillator();
+      squareOscillator.frequency.setValueAtTime(freq, 0);
+      squareOscillator.type = 'square';
+      squareOscillator.connect(squareGain);
+      squareOscillator.start();
 
       //Noise
       const buffer = audioContext.createBuffer(
